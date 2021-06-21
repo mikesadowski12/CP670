741d4d0bbcbc26d720c216667d6bfe8a31ccfa16
Mike Sadowski <msadowsk@opentext.com>
2021-06-20 20:04:40 -0400

    Finished Assignment2Part3

---
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -0,0 +1,108 @@
+package com.example.androidassignments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ChatWindow extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "ChatWindow";
+    ListView chatView;
+    EditText chatText;
+    Button sendButton;
+    ArrayList<String> chatMessages;
+    ChatAdapter messageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_window);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        chatView = findViewById(R.id.chatView);
+        chatText = findViewById(R.id.chatText);
+        sendButton = findViewById(R.id.sendButton);
+        chatMessages = new ArrayList<>();
+
+        messageAdapter = new ChatAdapter(this);
+        chatView.setAdapter(messageAdapter);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chatMessages.add(chatText.getText().toString());
+                messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/getView()
+                chatText.setText("");
+            }
+        });
+    }
+
+    private class ChatAdapter extends ArrayAdapter<String> {
+        public ChatAdapter(Context ctx) {
+            super(ctx, 0);
+        }
+
+        public int getCount() {
+            return chatMessages.size();
+        }
+
+        public String getItem(int position) {
+            return chatMessages.get(position);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result = null ;
+            if(position%2 == 0)
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+
+            TextView message = (TextView)result.findViewById(R.id.message_text);
+            message.setText(getItem(position)); // get the string at position
+            return result;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+}
